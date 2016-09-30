b79bbcf726b32be76300d83806b5197b16f4baf1
bijoyjraj <bijoyjraj@gmail.com>
2016-09-30 20:45:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,5 @@ console.log('Loaded!');
 
 // CHANGIN THE MAINT TEXT THE WAS GIVEN THE id 'text1'
 
-text = getElementById('text1');
+text = document.getElementById('text1');
 text.innerHTML = 'Hi, I am learning client side Javascript.';
