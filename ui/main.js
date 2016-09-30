dc73ba979326946770152e92ac017b5c23fd0cff
bijoyjraj <bijoyjraj@gmail.com>
2016-09-30 22:10:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 var Tout = setTimeout(alert("trying to introduce a delay of 1 second"),5000);
 clearTimeout(Tout);
-// CHANGIN THE MAINT TEXT THE WAS GIVEN THE id 'text1'
+// CHANGING THE MAIN TEXT, IT WAS GIVEN THE id 'text1'
 
 var text = document.getElementById('text1');
 text.innerHTML = 'Hi, I am learning client side Javascript.';
