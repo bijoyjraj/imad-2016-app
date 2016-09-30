29e70b2a3b056406002700037ca047158922c261
bijoyjraj <bijoyjraj@gmail.com>
2016-09-30 21:00:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,10 @@
 console.log('Loaded!');
 
+alert('Hi, I am trying out inline javascript!! ');
+
 // CHANGIN THE MAINT TEXT THE WAS GIVEN THE id 'text1'
 
 var text = document.getElementById('text1');
 text.innerHTML = 'Hi, I am learning client side Javascript.';
+
+alert('The text has changed!');
